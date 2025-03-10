e7e39ca6e7b25a7b6a1db0c95447ecaf04e91cf1
Alexandre Scordilis <ascordil@paul-f3Ar6s6.clusters.42paris.fr>
2025-03-10 16:51:22 +0100

    ajoutdotfile

---
+++ dotfiles/zsh/.config/zsh/aliases/aliases.zsh
@@ -0,0 +1,8 @@
+alias compile="cc -Wall -Wextra -Werror *.c -o diamond && ./diamond"
+alias francinette=/mnt/nfs/homes/ascordil/francinette/tester.sh
+alias nvim=/home/ascordil/appimage/nvim
+alias tmux=/home/ascordil/appimage/tmux
+alias stow=~/.local/bin/stow-bin/bin/stow
+alias config-zsh="vim ~/.zshrc"
+alias paco=/mnt/nfs/homes/ascordil/francinette/tester.sh
+export PATH=/mnt/nfs/homes/ascordil/.local/funcheck/host:$PATH
